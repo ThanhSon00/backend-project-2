a3f205273519ff3c37b989ba0e55635b86e36796
ThanhSon00 <phanson999999@gmail.com>
2023-06-10 16:00:21 +0700

    Add function to call api

--- setting/api.js
+++ setting/api.js
@@ -2,22 +2,32 @@ const axios = require('axios');
 const domain = process.env.DOMAIN;
 const protocol = process.env.PROTOCOL;
 const port = process.env.PORT;
-const timeout = process.env.TIMEOUT;
+const origin = `${protocol}://${domain}:${port}`;
 
-const url = `${protocol}://${domain}:${port}`;
-const restAPI = `${url}/api/v1`;
-
-const root = axios.create({
-    baseURL: url,
-    timeout
-})
-
-const api = axios.create({
-    baseURL: restAPI,
-    timeout
-})
+const makeRequest = async (path, method, body, callback) => {
+    let response, err;
+    try {
+        response = await axios({
+            url: `${origin}${path}`,
+            method: method,
+            data: body
+        });
+    } catch (error) {
+        err = error;
+    } finally {
+        if (typeof callback === 'function') {
+            return callback(err, response?.data);
+        }
+        return new Promise((resolve, reject) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(response.data);
+            }
+        })
+    }
+}
 
 module.exports = {
-    root,
-    api,
+    makeRequest,
 }
