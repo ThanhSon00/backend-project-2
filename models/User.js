7e16b9b006d2139f4efe84242ae014a2ecc5ea75
ThanhSon00 <phanson999999@gmail.com>
2023-06-10 15:27:56 +0700

    Add hook for encrypting password

--- models/User.js
+++ models/User.js
@@ -1,14 +1,27 @@
 const mongoose = require('../database/connect');
+const bcrypt = require('bcryptjs')
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: String,
     avatar: String,
     title: String,
     password: String,
-    phoneNumber: String,
+    email: { type: String, unique: true },
     groups: Array,
     notifications: Array
 });
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(user.password, salt);
+  user.password = hash;
+  user.selector = Math.floor(Math.random() * 9000000000) + 1000000000;
+  next();
+});
+
 
 const User = mongoose.model('user', userSchema);
 
