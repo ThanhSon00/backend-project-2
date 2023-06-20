272c1a0db9096f3ce164973e2bfc00da5277bd8b
ThanhSon00 <phanson999999@gmail.com>
2023-06-20 20:29:42 +0700

    Check if password reset link valid
    Check if link exist more than 1 hour

--- middleware/validateLockToken.js
+++ middleware/validateLockToken.js
@@ -2,6 +2,9 @@ const { makeRequest } = require("../setting/api");
 
 const validateLockToken = async (req, res, next) => {
     const { token } = req.params;
+    if (tokenExistMoreThan1Hour(token)) {
+        return res.redirect('back');
+    }
     makeRequest(`/api/v1/users?lockToken=${token}`, 'GET', null, (err, user) => {
         if (err) {
             return res.redirect('back');   
@@ -9,6 +12,14 @@ const validateLockToken = async (req, res, next) => {
         req.body.user = user;
         return next();
     });
+}
+
+const tokenExistMoreThan1Hour = (token) => {
+    const parts = token.split("-"); 
+    const createdTime = parts[parts.length - 1];
+    const currentTime = new Date().getTime();
+    const timeDifference = (currentTime - createdTime) / (1000 * 60 * 60);
+    return timeDifference > 1;
 }
 
 module.exports = validateLockToken;
