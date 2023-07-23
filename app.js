cf92e368e07817bec2dee83db0cd0d36c8974628
ThanhSon00 <phanson999999@gmail.com>
2023-07-23 11:34:06 +0700

    :hammer: Update App.js

--- app.js
+++ app.js
@@ -11,10 +11,14 @@ const authenticationRoutes = require('./routes/authentication');
 const userRoutes = require('./routes/api/user');
 const formRoutes = require('./routes/api/form');
 const sessionRoutes = require('./routes/api/session');
+const conversationRoutes = require('./routes/api/conversation')
+const chatLineRoutes = require('./routes/api/chatLine')
+
 const homeRoutes = require('./routes/home');
 const loginRoutes = require('./routes/login');
 const forgotPwdRoutes = require('./routes/forgotPassword');
 const resetPwdRoutes = require('./routes/resetPassword');
+const credentialRoutes = require('./routes/credential');
 
 // Middleware
 const { validateLockToken } = require('./middleware/validator');
@@ -38,11 +42,16 @@ app.use('/authentication', authenticationRoutes);
 app.use('/home', homeRoutes);
 app.use('/forgot-password', forgotPwdRoutes);
 app.use('/reset-password/:token', validateLockToken, resetPwdRoutes);
+app.use('/whoami', credentialRoutes);
 
 // Rest api
 app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/sessions', sessionRoutes);
 app.use('/api/v1/forms', formRoutes);
+app.use('/api/v1/conversations', conversationRoutes);
+app.use('/api/v1/chat-lines', chatLineRoutes);
+
+// Normal api
+app.use('/api/v1/sessions', sessionRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,7 +64,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  winstonLogger.error(err);
+  // winstonLogger.error(err);
   console.log(err);
   // render the error page
   res.status(err.status || 500);
