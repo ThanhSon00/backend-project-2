4fa83ccaed9a066dc8cecb7b5a189d8e5d1f65ea
ThanhSon00 <phanson999999@gmail.com>
2023-08-18 21:08:28 +0700

    :bug: Fix bug

--- database/connect.js
+++ database/connect.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
 if (process.env.ENV === "production") {
-    mongoose.connect(process.env.DATABASE);
+    mongoose.connect(process.env.DATABASE).then(() => {
+        console.log("Connect to atlas successfully");
+    }).catch((e) => {
+        console.error(e);
+    })
 } else if (process.env.ENV === "development") {
-    mongoose.connect('mongodb://localhost:27017/test');
+    mongoose.connect('mongodb://127.0.0.1/test').then(()=>{
+        console.log(`successfully connected`);
+    }).catch((e)=>{
+        console.log(e);
+    });
 }
 
 module.exports = mongoose
